62f28a33254058954d1aa8d20324e6f53f49f20e
Adeeb R <adeebsr700@gmail.com>
2023-12-03 23:03:21 +0900

    Format code to be more readable

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import pygame
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 import os
 
 bgBlack = "#171D1C"
@@ -11,14 +11,17 @@ screen = Tk()
 screen.title("Melodia")
 screen.configure(bg=bgBlack)
 
-#Works for windows
-#screen.iconbitmap("")
+# Works for windows
 
-#Works for linux
-img = PhotoImage(file='./sources/icon.gif')
-screen.tk.call('wm', 'iconphoto', screen._w, img)
+# screen.iconbitmap("./sources/icon.ico")
+
+# Works for linux
+img = PhotoImage(file="./sources/icon.gif")
+
+screen.tk.call("wm", "iconphoto", screen._w, img)
 screen.iconphoto(True, img)
-screen.resizable(0,0)
+
+screen.resizable(0, 0)
 
 pygame.mixer.init()
 
@@ -33,37 +36,20 @@ backBtnImg = PhotoImage(file="./sources/ctrlbtn/back.png")
 global playState
 playState = 0
 
-# def play():
-#     print("Play pressed")
-#     pygame.mixer.music.load("./music/music.mp3")
-#     pygame.mixer.music.play(loops=0)
 
 def changeCover(trackNum):
-    # albumCover = trackBox.get(ACTIVE)
-    # albumCover = albumCover.replace(" ", "_")
-    # albumCover = albumCover.replace(".mp3", "")
-    # albumCover = f"./music/albumCover/{albumCover}-cover.jpg"
-    # #print(f"ALBUM COVER FILE NAME : {albumCover}")
-
-    # curCover = Image.open(albumCover)
-    # curCover = curCover.resize((300,300), Image.ANTIALIAS)
-    # curCover = ImageTk.PhotoImage(curCover)
-
-    # curCoverLabel.configure(image=curCover) 
-
-    #####
     global curCover
 
     albumCover = tracks[trackNum]
     albumCover = albumCover.replace(".mp3", "")
     albumCover = f"./music/albumCover/{albumCover}-cover.jpg"
 
-
     curCover = Image.open(albumCover)
-    curCover = curCover.resize((250,250), Image.ANTIALIAS)
+    curCover = curCover.resize((250, 250), Image.ANTIALIAS)
     curCover = ImageTk.PhotoImage(curCover)
 
     curCoverLabel.configure(image=curCover)
+
 
 def stop():
     print("Stop pressed")
@@ -75,12 +61,14 @@ def stop():
     mainBtn.configure(image=playBtnImg)
     mainBtn.photo = playBtnImg
 
+
 def mainBtnFunc(mainQuery):
     global playState
     global tracks
     mainQuery = playState
 
     if playState == 0:
+        pygame.mixer.music.pause()
         print("Play pressed first time")
 
         track = trackBox.get(ACTIVE)
@@ -89,22 +77,20 @@ def mainBtnFunc(mainQuery):
         track = f"./music/{track}.mp3"
         trackIndex = tracks.index(track.replace("./music/", ""))
 
-
         pygame.mixer.music.load(track)
         pygame.mixer.music.play(loops=0)
         playstate = 1
 
         changeCover(trackIndex)
 
-
-    if playState == 1 :
+    if playState == 1:
         pygame.mixer.music.pause()
         print("Paused")
         playState = 2
 
         mainBtn.configure(image=playBtnImg)
         mainBtn.photo = playBtnImg
-        
+
     else:
         pygame.mixer.music.unpause()
         print("Unpaused")
@@ -113,18 +99,18 @@ def mainBtnFunc(mainQuery):
         mainBtn.configure(image=pauseBtnImg)
         mainBtn.photo = pauseBtnImg
 
+
 def nextTrack(move):
     global playState
 
     curTrack = trackBox.curselection()[0]
 
     if curTrack == 0 and move == -1:
-        nextTrack = trackBox.size()-1
-    elif curTrack == (trackBox.size()-1) and move == 1:
+        nextTrack = trackBox.size() - 1
+    elif curTrack == (trackBox.size() - 1) and move == 1:
         nextTrack = 0
     else:
-        nextTrack = trackBox.curselection()[0]+move
-
+        nextTrack = trackBox.curselection()[0] + move
 
     print("Current song is number", nextTrack)
     playTrack = trackBox.get(nextTrack)
@@ -133,30 +119,26 @@ def nextTrack(move):
 
     trackBox.selection_clear(0, END)
     trackBox.activate(nextTrack)
-    trackBox.selection_set(nextTrack,last=None)  
+    trackBox.selection_set(nextTrack, last=None)
 
-    changeCover(nextTrack)  
+    changeCover(nextTrack)
 
     playState = 0
     mainBtnFunc(0)
 
-# def prevTrack():
-#     nextTrack = trackBox.curselection()[0]-move
-#     # print("Current song is number", nextTrack)
-#     playTrack = trackBox.get(nextTrack)
-#     playTrack = playTrack.replace(" ", "_")
-#     playTrack = f"./music/{playTrack}.mp3"
 
-#     trackBox.selection_clear(0, END)
-#     trackBox.activate(nextTrack)
-#     trackBox.selection_set(nextTrack,last=None)
-
-#     mainBtnFunc(0)
-
-trackBox = Listbox(screen, bg="#171D1C",fg=fgWhite,width=30, height=5,borderwidth=0, highlightthickness=0,  selectbackground=AccentBlue, selectborderwidth=0)
+trackBox = Listbox(
+    screen,
+    bg="#171D1C",
+    fg=fgWhite,
+    width=30,
+    height=5,
+    borderwidth=0,
+    highlightthickness=0,
+    selectbackground=AccentBlue,
+    selectborderwidth=0,
+)
 trackBox.activate(0)
-# trackBox.selection_set( first = 0 )
-# trackBox.select_set(0)
 
 global tracks
 tracks = []
@@ -172,7 +154,7 @@ tracks = sorted(tracks)
 for name in tracks:
     name = name.replace(".mp3", "")
     name = name.replace("_", " ")
-    trackBox.insert('end', name)
+    trackBox.insert("end", name)
 
 albumCover = tracks[0]
 albumCover = albumCover.replace(".mp3", "")
@@ -181,10 +163,10 @@ albumCover = f"./music/albumCover/{albumCover}-cover.jpg"
 global curCover
 
 curCover = Image.open(albumCover)
-curCover = curCover.resize((250,250), Image.ANTIALIAS)
+curCover = curCover.resize((250, 250), Image.ANTIALIAS)
 curCover = ImageTk.PhotoImage(curCover)
 
-curCoverLabel = Label(image=curCover,borderwidth=0,highlightthickness = 0)
+curCoverLabel = Label(image=curCover, borderwidth=0, highlightthickness=0)
 curCoverLabel.pack(pady=20)
 
 trackBox.pack()
@@ -192,26 +174,47 @@ trackBox.pack()
 btnDiv = Frame(screen, bg=bgBlack, pady=10, bd=0)
 btnDiv.pack()
 
-mainBtn = Button(btnDiv, image=playBtnImg , borderwidth=0, command=lambda:mainBtnFunc(playState),bg=bgBlack,highlightthickness = 0, bd = 0)
-stopBtn = Button(btnDiv, image=stopBtnImg , borderwidth=0, command=stop,bg=bgBlack,highlightthickness = 0, bd = 0)
-backBtn = Button(btnDiv, image=backBtnImg , borderwidth=0, command=lambda:nextTrack(-1),bg=bgBlack,highlightthickness = 0, bd = 0)
-frontBtn = Button(btnDiv, image=frontBtnImg , borderwidth=0, command=lambda:nextTrack(1),bg=bgBlack,highlightthickness = 0, bd = 0)
+mainBtn = Button(
+    btnDiv,
+    image=playBtnImg,
+    borderwidth=0,
+    command=lambda: mainBtnFunc(playState),
+    bg=bgBlack,
+    highlightthickness=0,
+    bd=0,
+)
+stopBtn = Button(
+    btnDiv,
+    image=stopBtnImg,
+    borderwidth=0,
+    command=stop,
+    bg=bgBlack,
+    highlightthickness=0,
+    bd=0,
+)
+backBtn = Button(
+    btnDiv,
+    image=backBtnImg,
+    borderwidth=0,
+    command=lambda: nextTrack(-1),
+    bg=bgBlack,
+    highlightthickness=0,
+    bd=0,
+)
+frontBtn = Button(
+    btnDiv,
+    image=frontBtnImg,
+    borderwidth=0,
+    command=lambda: nextTrack(1),
+    bg=bgBlack,
+    highlightthickness=0,
+    bd=0,
+)
 
-backBtn.grid(row=0,column=0, padx=20)
-mainBtn.grid(row=0,column=1, padx=20)
-frontBtn.grid(row=0,column=2, padx=20)
+backBtn.grid(row=0, column=0, padx=20)
+mainBtn.grid(row=0, column=1, padx=20)
+frontBtn.grid(row=0, column=2, padx=20)
 
-stopBtn.grid(row=1,column=1, pady=10)
-
-# pauseBtn = Button(btnDiv, image=pauseBtnImg , borderwidth=0,command=lambda:mainBtn(mainQuery)) 
-# pauseBtn.grid(row=0,column=2, padx=10)
-
-
-
-#playbtn = Button(screen, text="Play",font=("./sources/Roboto-Black.ttf", 32), command=play)
-#playbtn.pack(pady=20)
-
-#stopbtn = Button(screen, text="Stop",font=("./sources/Roboto-Black.ttf", 32), command=stop)
-#stopbtn.pack(pady=20)
+stopBtn.grid(row=1, column=1, pady=10)
 
 screen.mainloop()
